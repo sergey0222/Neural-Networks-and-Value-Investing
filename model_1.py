8d0625b2c3302cafadf6b2cf1eaca082e7db7c9b
Sergey <47866942+sergey0222@users.noreply.github.com>
2019-04-19 18:18:03 +0300

    Update model_1.py

--- model_1.py
+++ model_1.py
@@ -14,12 +14,13 @@ import math
 
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.regularizers import l1
 
 # store starting time for calculating total processing time
 time_start = datetime.datetime.now()
 
 # set path to folder Datasets/
-path = 'D:/DataSets/'
+path = 'C:/Users/belyisn.N0COSA/Desktop/Files/DataSets/'
 
 # the tag to be used for input data
 tag_org = 'NetIncomeLoss'
@@ -41,17 +42,30 @@ XY_bool = np.zeros((len(index_cik), last-first+1), dtype = bool)
 XY_float = np.zeros((len(index_cik), last-first+1), dtype = float)
 
 # create empty array which will hold cik and FY year for each adsh
-adsh_cik_year = np.zeros((len(index_adsh),2), dtype=int)
+adsh_cik_year = np.zeros((len(index_adsh),3), dtype=int)
 
-# for each adsh get cik and Full Report Year
+# for each adsh get cik, sic and Full Report Year
+# as sic should be reindexed initialize needed objects
+sic_dic = {}
+next_available_index = 0
+
 with open(path + 'filter_1/filter_1_sub.txt') as f:
         f_object = csv.reader(f, delimiter='\t')
         for row in f_object:
             adsh_int = int(row[0])
             cik_int = int(row[1])
+            
+            sic_str = row[3]
+            index, next_available_index = ds.index_by_tree (sic_str, sic_dic, next_available_index)
+            
             fy_int = int(row[27])
+            
             adsh_cik_year[adsh_int,0] = cik_int
             adsh_cik_year[adsh_int,1] = fy_int
+            adsh_cik_year[adsh_int,2] = index
+
+# create array for keeping sic one hot vectors
+XY_onehot = np.zeros((len(index_cik), next_available_index), dtype = float)            
 
 # look through filter_1_num.txt for all eppearances of the tag
 with open(path + 'filter_1/filter_1_num.txt') as f:
@@ -77,6 +91,9 @@ with open(path + 'filter_1/filter_1_num.txt') as f:
                
                # write down the correpsonding value
                XY_float[r,c] = value_float
+               
+               index = adsh_cik_year[adsh_int,2]
+               XY_onehot[r,index] = 1
 
 # define rows where values exist for all years in a range
 mask1D = np.all(XY_bool, axis=1, keepdims = True)
@@ -86,58 +103,71 @@ elig_cik_list = np.nonzero(mask1D)[0]
 
 # extact only eligible rows
 XY = XY_float[elig_cik_list,:]
+XY_onehot = XY_onehot[elig_cik_list,:]
 
-# normalize XY
-mean = np.mean(XY, axis=1, keepdims=True)
-XY_centered = XY - mean
-maximum = np.max(np.abs(XY_centered), axis=1, keepdims=True)
-XY_normilized = XY_centered / maximum
+# shuffle the array
+#np.random.shuffle(XY)
+
+# define a year to predict
+year_to_predict = 2018
+
+# cut only needed years
+XY_cut = XY[:,0:(year_to_predict - first + 1)]
+
+X = XY_cut[:,0:-1]
+Y = XY_cut[:,-1:]
+
+# normalize X (using X data only!)
+mean = np.mean(X, axis=1, keepdims=True)
+X_centered = X - mean
+maximum = np.max(np.abs(X_centered), axis=1, keepdims=True)
+X_normilized = X_centered / maximum + 10
+X_onehot_normilized = np.concatenate((XY_onehot,X_normilized),axis=1)
+
+# normilize Y using X data (not Y!)
+Y_normilized = (Y - mean) / maximum + 10
 
 # splet between train and dev sets
 # calculate number of traning examples
-m = np.shape(XY_normilized)[0]   
-
+m = np.shape(X_normilized)[0]   
 # set percentage for test set
 test_percent = 10
 m_test = math.floor(test_percent/100*m)
 m_train = m - m_test
 
-# shuffle the array
-np.random.shuffle(XY_normilized)
-
 # Input and labels for neural network
-X_train = XY_normilized[0:m_train, 0:-1]
-Y_train = XY_normilized[0:m_train, -1:]
+X_train = X_onehot_normilized[0:m_train,:]
+Y_train = Y_normilized[0:m_train,:]
 
-X_test = XY_normilized[m_train:, 0:-1]
-Y_test = XY_normilized[m_train:, -1:]
+X_test = X_onehot_normilized[m_train:,:]
+Y_test = Y_normilized[m_train:,:]
  
 # build a model
 model = Sequential()
-model.add(Dense(64, activation='relu', input_shape=(np.shape(X_train)[1],)))
-model.add(Dense(64, activation='relu'))
-model.add(Dense(32, activation='relu'))
+model.add(Dense(64, activation='relu', kernel_regularizer=l1(0.001), input_shape=(np.shape(X_train)[1],)))
+model.add(Dense(64, activation='relu', kernel_regularizer=l1(0.001)))
+model.add(Dense(32, activation='relu', kernel_regularizer=l1(0.001)))
 model.add(Dense(1))
-
-model.compile(loss='mean_absolute_error', optimizer='Adam')
-
+model.compile(loss='mean_absolute_error', optimizer='RMSProp')
 model.fit(X_train, Y_train, epochs=1000, batch_size=32)
 
 # evaluate result
 loss = model.evaluate(X_test, Y_test)
 print('Test set loss is:', loss)
 
-body = np.mean(np.abs(Y_test))
-print('Error on a test set is', round(loss/body*100,1), '%')
+#body = np.mean(np.abs(Y_test))
+#print('Error on a test set is', round(loss/body*100,1), '%')
 
+'''
 # let us examine the results manually
 # define which test example to choose
-test_example = 133
+test_example = 13
 print('Test example input is:', X_test[test_example])
 print('Test example output is', Y_test[test_example])
 
 prediction = model.predict(X_test)
 print('Prediction is:', prediction[test_example,0])   
+'''
 
 # processing time
 print('time elapsed - ', datetime.datetime.now() - time_start)    
